78fb23970dcb7d065b75352a2b24a1b855712ff5
Khalid J Hosein <khosein@2ndwatch.com>
2021-12-13 18:24:03 -0500

    Test with incorrect secrets.

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket_object" "folder" {
 
 resource "aws_s3_bucket_object" "file8" {
   bucket = "kjh-test-bucket-tagging"
-  key    = "foldertest2/keep8"
+  key    = "foldertest2/keep9"
   source = "/dev/null"
 }
 
