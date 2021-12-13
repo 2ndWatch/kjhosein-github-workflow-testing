2c32106b569077321b037de2762e992086991af8
Khalid J Hosein <khosein@2ndwatch.com>
2021-12-13 17:52:56 -0500

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,19 @@
+# S3 object creation
+
+resource "aws_s3_bucket_object" "folder" {
+  bucket = "kjh-test-bucket-tagging"
+  key    = "foldertest2/.keep"
+  source = "/dev/null"
+}
+
+resource "aws_s3_bucket_object" "file3" {
+  bucket = "kjh-test-bucket-tagging"
+  key    = "foldertest2/keep6"
+  source = "/dev/null"
+}
+
+# Outputs
+output "folder_id" {
+  value       = "Folder id is ${aws_s3_bucket_object.folder.id}"
+  description = "ID of the S3 object"
+}
---
+++ setup.tf
@@ -0,0 +1,29 @@
+
+# AWS Region where this application will be deployed:
+variable "aws_region" {}
+# variable "aws_profile" {}
+
+provider "aws" {
+  region  = var.aws_region
+  # profile = var.aws_profile
+}
+
+
+terraform {
+  # required_version = ">= 0.13.5, < 0.14.0"
+  required_version = "~> 0.13.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.50.0, < 4.0.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "kjh-test-bucket-tagging"
+    key     = "terraform/tf-s3-object.state"
+    region  = "us-east-1"
+    profile = "kjh"
+  }
+}
